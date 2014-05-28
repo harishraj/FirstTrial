15e4772d4a6c32259a5762f251f68bace7ffcd02
rajan.rajendra <rajan.rajendra@tubemogul.com>
2014-05-27 20:30:37 -0700

    Update

--- utilities/LRUCache.java
+++ utilities/LRUCache.java
@@ -1,18 +1,17 @@
 import java.util.HashMap;
- 
+
 public class LRUCache {
-	private HashMap<Integer, DoubleLinkedListNode> map 
-		= new HashMap<Integer, DoubleLinkedListNode>();
+	private HashMap<Integer, DoubleLinkedListNode> map = new HashMap<Integer, DoubleLinkedListNode>();
 	private DoubleLinkedListNode head;
 	private DoubleLinkedListNode end;
 	private int capacity;
 	private int len;
- 
+
 	public LRUCache(int capacity) {
 		this.capacity = capacity;
 		len = 0;
 	}
- 
+
 	public int get(int key) {
 		if (map.containsKey(key)) {
 			DoubleLinkedListNode latest = map.get(key);
@@ -23,38 +22,38 @@ public class LRUCache {
 			return -1;
 		}
 	}
- 
+
 	public void removeNode(DoubleLinkedListNode node) {
 		DoubleLinkedListNode cur = node;
 		DoubleLinkedListNode pre = cur.pre;
 		DoubleLinkedListNode post = cur.next;
- 
+
 		if (pre != null) {
 			pre.next = post;
 		} else {
 			head = post;
 		}
- 
+
 		if (post != null) {
 			post.pre = pre;
 		} else {
 			end = pre;
 		}
 	}
- 
+
 	public void setHead(DoubleLinkedListNode node) {
 		node.next = head;
 		node.pre = null;
 		if (head != null) {
 			head.pre = node;
 		}
- 
+
 		head = node;
 		if (end == null) {
 			end = node;
 		}
 	}
- 
+
 	public void set(int key, int value) {
 		if (map.containsKey(key)) {
 			DoubleLinkedListNode oldNode = map.get(key);
@@ -62,8 +61,7 @@ public class LRUCache {
 			removeNode(oldNode);
 			setHead(oldNode);
 		} else {
-			DoubleLinkedListNode newNode = 
-				new DoubleLinkedListNode(key, value);
+			DoubleLinkedListNode newNode = new DoubleLinkedListNode(key, value);
 			if (len < capacity) {
 				setHead(newNode);
 				map.put(key, newNode);
@@ -74,20 +72,20 @@ public class LRUCache {
 				if (end != null) {
 					end.next = null;
 				}
- 
+
 				setHead(newNode);
 				map.put(key, newNode);
 			}
 		}
 	}
 }
- 
+
 class DoubleLinkedListNode {
 	public int val;
 	public int key;
 	public DoubleLinkedListNode pre;
 	public DoubleLinkedListNode next;
- 
+
 	public DoubleLinkedListNode(int key, int value) {
 		val = value;
 		this.key = key;
